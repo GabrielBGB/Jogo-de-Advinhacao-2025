8f7fc50827e7476affba3fd7e46d5a1877bf91ff
GabrielBGB <gabrielprogramacao@outlook.com.br>
2025-04-10 19:33:42 -0300

    Finalizado a refatoração do codigo e testado rodando sem erros ou bug

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -1,85 +1,114 @@
-﻿while (true)
+﻿using System;
+
+class Program
 {
-    Console.Clear();
-    Console.WriteLine("----------------------------------------");
-    Console.WriteLine("Jogo de Adivinhação");
-    Console.WriteLine("----------------------------------------");
+    static void Main()
+    {
+        while (true)
+        {
+            MostrarCabecalho("Jogo de Adivinhação");
 
-    // Escolha de dificuldade
-    Console.WriteLine("Escolha um nível de dificuldade:");
-    Console.WriteLine("----------------------------------------");
-    Console.WriteLine("1 - Fácil (10 tentativas)");
-    Console.WriteLine("2 - Médio (5 tentativas)");
-    Console.WriteLine("3 - Difícil (3 tentativas)");
-    Console.WriteLine("----------------------------------------");
+            int totalDeTentativas = EscolherDificuldade();
 
-    int totalDeTentativas = 0;
+            int numeroSecreto = new Random().Next(1, 21);
 
-    Console.Write("Digite sua escolha: ");
-    string entrada = Console.ReadLine();
+            Jogar(numeroSecreto, totalDeTentativas);
 
-    if (entrada == "1")
-        totalDeTentativas = 10;
-    else if (entrada == "2")
-        totalDeTentativas = 5;
-    else if (entrada == "3")
-        totalDeTentativas = 3;
+            Console.Write("Deseja continuar? (S/N): ");
+            string opcao = Console.ReadLine().ToUpper();
 
-    // Geração de número secreto
-    Random geradorDeNumeros = new Random();
+            if (opcao != "S")
+                break;
+        }
+    }
 
-    int numeroSecreto = geradorDeNumeros.Next(1, 21);
+    #region Métodos Auxiliares
 
-    // Loop de tentativas do jogo
-    for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
+    static void MostrarCabecalho(string titulo)
     {
         Console.Clear();
         Console.WriteLine("----------------------------------------");
-        Console.WriteLine($"Tentativa {tentativa} de {totalDeTentativas}");
+        Console.WriteLine(titulo);
         Console.WriteLine("----------------------------------------");
-
-        // Lógica do jogo
-        Console.Write("Digite um número entre 1 e 20: ");
-        int numeroDigitado = Convert.ToInt32(Console.ReadLine());
-
-        if (numeroDigitado == numeroSecreto)
-        {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Parabéns! Você acertou!");
-            Console.WriteLine("----------------------------------------");
-
-            break;
-        }
-
-        if (tentativa == totalDeTentativas)
-        {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine($"Que pena! Você usou todas as tentativas. O número era {numeroSecreto}.");
-            Console.WriteLine("----------------------------------------");
-
-            break;
-        }
-
-        else if (numeroDigitado > numeroSecreto)
-        {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("O número digitado é maior que o número secreto.");
-            Console.WriteLine("----------------------------------------");
-        }
-        else
-        {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("O número digitado é menor que o número secreto.");
-            Console.WriteLine("----------------------------------------");
-        }
-
-        Console.WriteLine("Aperte ENTER para continuar...");
-        Console.ReadLine();
     }
 
-    Console.Write("Deseja continuar? (S/N): ");
-    string opcaoContinuar = Console.ReadLine().ToUpper();
+    static void MostrarMensagem(string mensagem)
+    {
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine(mensagem);
+        Console.WriteLine("----------------------------------------");
+    }
 
-    if (opcaoContinuar != "S")
-        break;
+    #endregion
+
+    #region Lógica do Jogo
+
+    static void Jogar(int numeroSecreto, int totalDeTentativas)
+    {
+        for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
+        {
+            MostrarCabecalho($"Tentativa {tentativa} de {totalDeTentativas}");
+
+            Console.Write("Digite um número entre 1 e 20: ");
+            int chute = LerNumero();
+
+            if (chute == numeroSecreto)
+            {
+                MostrarMensagem("Parabéns! Você acertou!");
+                break;
+            }
+
+            if (tentativa == totalDeTentativas)
+            {
+                MostrarMensagem($"Que pena! Você usou todas as tentativas. O número era {numeroSecreto}.");
+                break;
+            }
+
+            if (chute > numeroSecreto)
+                MostrarMensagem("O número digitado é maior que o número secreto.");
+            else
+                MostrarMensagem("O número digitado é menor que o número secreto.");
+
+            Console.WriteLine("Aperte ENTER para continuar...");
+            Console.ReadLine();
+        }
+    }
+
+    static int LerNumero()
+    {
+        while (true)
+        {
+            string entrada = Console.ReadLine();
+            if (int.TryParse(entrada, out int numero) && numero >= 1 && numero <= 20)
+                return numero;
+
+            Console.Write("Entrada inválida. Digite um número entre 1 e 20: ");
+        }
+    }
+
+    #endregion
+
+    #region Escolha de Dificuldade
+
+    static int EscolherDificuldade()
+    {
+        Console.WriteLine("Escolha um nível de dificuldade:");
+        Console.WriteLine("1 - Fácil (10 tentativas)");
+        Console.WriteLine("2 - Médio (5 tentativas)");
+        Console.WriteLine("3 - Difícil (3 tentativas)");
+        Console.WriteLine("----------------------------------------");
+
+        Console.Write("Digite sua escolha: ");
+        string entrada = Console.ReadLine();
+
+        return entrada switch
+        {
+            "1" => 10,
+            "2" => 5,
+            "3" => 3,
+            _ => 5 // padrão: médio
+        };
+    }
+
+    #endregion
 }
